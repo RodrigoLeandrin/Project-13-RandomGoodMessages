00cfff59c7378657e32220182a9a93d62a06f09e
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-12-25 23:31:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 alert ("working!");
-const messages = [];
-const backgounds = [];
+
+
+function generate(){
+    var quotes = {
+        "-Jesus Christ" : '"For everyone who exalts himself will be humbled,and everyone who humbles himself will be exalted."',
+        "- Jesus Christ" : '"So I say to you, Ask and it will be given to you; search, and you will find; knock, and the door will be opened for you."',
+
+    }
+
+
+var authors = Object.keys(quotes);
+console.log(authors);
+var author = authors[Math.floor(Math.random()*authors.length)];
+var quote = quotes[author];
+
+document.querySelector("h1").innerHTML=quote;
+
+document.querySelector("h2").innerHTML=author;
+}
