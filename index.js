048a43b005c9220b485725ebc9f64d4613294b2a
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-12-29 13:45:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ alert ("working!");
 
 function generate(){
     var quotes = {
-        "-Jesus Christ" : '"For everyone who exalts himself will be humbled,and everyone who humbles himself will be exalted."',
-        "- Jesus Christ" : '"So I say to you, Ask and it will be given to you; search, and you will find; knock, and the door will be opened for you."',
-
+        "-1 Jesus Christ" : '"For everyone who exalts himself will be humbled,and everyone who humbles himself will be exalted."',
+        "-2 Jesus Christ" : '"So I say to you, Ask and it will be given to you; search, and you will find; knock, and the door will be opened for you."',
+        "- 3 Jesus Christ" : '"As the Father has loved me, so have I loved you."',
+        "-4 Jesus Christ" : '"It is not the healthy who need a doctor, but the sick. I have not come to call the righteous, but sinners to repentance."',
+       "-5 Jesus Christ" : '"And know that I am with you always; yes, to the end of time."',
+     
     }
 
 
